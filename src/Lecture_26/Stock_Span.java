d71e9b23454393e3b9d903da5943fdc2539fad08
Ritesh Kumar <riteshkumar051204@gmail.com>
2026-06-06 00:49:09 +0530

    Stock span Monotonic Stack

---
+++ src/Lecture_26/Stock_Span.java
@@ -0,0 +1,30 @@
+package Lecture_26;
+
+import java.util.Stack;
+
+public class Stock_Span {
+    public static void main(String[] args) {
+        int[] arr={30,35,40,38,35};
+        Cal_soan(arr);
+    }
+    public static void  Cal_soan(int[]arr){
+        Stack<Integer>st=new Stack<>();
+        int[]ans=new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            while (!st.isEmpty() && arr[i]>=arr[st.peek()]){
+                st.pop();
+            }
+            //calculate span
+            if (st.isEmpty()){
+                ans[i]=i+1;
+            }else {
+                ans[i]=i-st.peek();
+            }
+            st.push(i);
+        }
+        for (int i = 0; i < ans.length; i++) {
+            System.out.print(ans[i]+" ");
+        }
+
+    }
+}
